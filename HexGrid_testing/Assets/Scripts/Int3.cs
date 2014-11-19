9137ebedb2f15174e5813b3266dfc9bd1f7e6465
Cody Blower <codymblower@gmail.com>
2014-11-19 12:44:32 -0800

    Scripts Update - PathFinding

---
+++ HexGrid_testing/Assets/Scripts/Int3.cs
@@ -0,0 +1,16 @@
+﻿[System.Serializable]
+public struct Int3 {
+	public int x, y, z;
+
+	public Int3(int x, int y, int z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public static Int3 operator + (Int3 a, Int3 b){
+		a.x += b.x;
+		a.y += b.y;
+		a.z += b.z;
+		return a;
+	}
+}
